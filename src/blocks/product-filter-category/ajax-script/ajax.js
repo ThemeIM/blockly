329b7ba0665b35cb06924df168d1bf1c33740318
Md Nayeem Farid <39758380+nayeem07007@users.noreply.github.com>
2022-06-20 11:13:04 +0600

    update

--- src/blocks/product-filter-category/ajax-script/ajax.js
+++ src/blocks/product-filter-category/ajax-script/ajax.js
@@ -1,19 +1,22 @@
 ;jQuery(function($){
     "user strict"
-    $(document).on('click', '.res-nav-tab  button, .res-nav-tab-two button', function(e){
-        e.preventDefault();
+
+    let getCurrentCat = $('.get-cat-value').val();
+    let getCurrentCatFlist = $('.res-nav-tab-three button.active').data('cat');
+    let CurrentCatId = getCurrentCat ? getCurrentCat : getCurrentCatFlist;
+    console.log(CurrentCatId);
+
         $('.ajax-preloader').fadeIn();
         var data = {
-            'action': 'blockly_product_filter',
-            'nonce': blockly_product_filter.nonce,     // We pass php values differently!
-            'cat' : $(this).data('cat')
+            'action': 'blockly_product_filter_cat',
+            'nonce': blockly_product_cat_filter.nonce,     // We pass php values differently!
+            'cat' : CurrentCatId
         };
-        jQuery.post(blockly_product_filter.ajaxurl, data, function(response) {
-            console.log(response);
-             $('.ajax-content-load').html(response);
+        jQuery.post(blockly_product_cat_filter.ajaxurl, data, function(response) {
+            console.log($('.ajax-content-loader'));
+             $('.ajax-content-loader').html(response);
              $('.ajax-preloader').fadeOut();;
         });
-    });
 });
 
 
