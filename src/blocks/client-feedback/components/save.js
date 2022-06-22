782b68ff9a5701c5bb81c07ed3a62e7fd04f3812
Md Nayeem Farid <mdnayeemfarid@Md-MacBoo-k-Air.local>
2022-06-22 19:06:46 +0600

    Update

--- src/blocks/client-feedback/components/save.js
+++ src/blocks/client-feedback/components/save.js
@@ -1,38 +1,37 @@
 import ReviewItem from "./reviewItem";
 import SliderNext from "./sliderNext";
 import SliderPrev from "./sliderPrev";
-import { useBlockProps } from '@wordpress/block-editor';
 
-
-export default function Save({ attributes }) {
+export default function Save({ attributes, setAttributes }) {
     const { title, subtitle, video_thumbnail, video_url, items } = attributes;
-    const blockProps = useBlockProps.save();
-//class="client-section bg--gray ptb-120 blockly-full"
-    return (
-        <div { ...blockProps({className: 'client-section bg--gray ptb-120 blockly-full',}) }>
-            <div class="client-element">
+
+    return ( 
+        <>
+        <div>
+        <div className="client-section bg--gray ptb-120 blockly-full">
+            <div className="client-element">
                 <img src={`${ blocklyBlockData['blockly_url'] }assets/images/element/element-19.png`} alt="element" />
             </div>
-            <div class="container">
-                <div class="row">
-                    <div class="col-xl-12">
-                        <div class="section-header-wrapper">
-                            <div class="section-header">
-                                <h2 class="section-title">{ title }</h2>
+            <div className="container">
+                <div className="row">
+                    <div className="col-xl-12">
+                        <div className="section-header-wrapper">
+                            <div className="section-header">
+                                <h2 className="section-title">{ title }</h2>
                                 <p>{ subtitle }</p>
                             </div>
-                            <div class="slider-nav-area">
+                            <div className="slider-nav-area">
                                 <SliderPrev />
-                                <SliderNext />
+                                <SliderNext /> 
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="client-area">
-                    <div class="row justify-content-center align-items-end mb-30-none">
-                        <div class="col-xl-8 col-lg-8 col-md-6 mb-30">
-                            <div class="client-slider">
-                                <div class="swiper-wrapper">
+                <div className="client-area">
+                    <div className="row justify-content-center align-items-end mb-30-none">
+                        <div className="col-xl-8 col-lg-8 col-md-6 mb-30">
+                            <div className="client-slider">
+                                <div className="swiper-wrapper">
                                     {
                                         typeof items.map === 'function' && items.map((item, i) => (
                                             <ReviewItem
@@ -46,28 +45,28 @@ export default function Save({ attributes }) {
                                 </div>
                             </div>
                         </div>
-                        <div class="col-xl-4 col-lg-4 col-md-6 mb-30">
-                            <div class="client-right-thumb">
+                        <div className="col-xl-4 col-lg-4 col-md-6 mb-30">
+                            <div className="client-right-thumb">
                                 <img
                                     src={video_thumbnail}
                                     alt="client"
                                 />
-                                <div class="client-thumb-overlay">
-                                    <div class="client-thumb-video">
-                                        <div class="video-main">
-                                            <div class="promo-video">
-                                                <div class="waves-block">
-                                                    <div class="waves wave-1"></div>
-                                                    <div class="waves wave-2"></div>
-                                                    <div class="waves wave-3"></div>
+                                <div className="client-thumb-overlay">
+                                    <div className="client-thumb-video">
+                                        <div className="video-main">
+                                            <div className="promo-video">
+                                                <div className="waves-block">
+                                                    <div className="waves wave-1"></div>
+                                                    <div className="waves wave-2"></div>
+                                                    <div className="waves wave-3"></div>
                                                 </div>
                                             </div>
                                             <a
-                                                class="video-icon"
+                                                className="video-icon"
                                                 data-rel="lightcase:myCollection"
                                                 href={video_url}
                                             >
-                                                <i class="fas fa-play"></i>
+                                                <i className="fas fa-play"></i>
                                             </a>
                                         </div>
                                     </div>
@@ -78,5 +77,7 @@ export default function Save({ attributes }) {
                 </div>
             </div>
         </div>
+        </div>
+        </>
     )
 }
